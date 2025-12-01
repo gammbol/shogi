7d18a8531ae2c3c90aeaab6480539301183820a1
sofixlu <sofia.luky@icloud.com>
2025-12-01 21:47:14 +0300

    Перенесла логику доски из Program.cs в Board.cs

---
+++ Board.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shogi
+{
+    class Board
+    {
+        static void Main()
+        {
+            const int size = 9;
+            string[,] board = new string[size, size];
+
+            // Заполняем поле пустыми клетками
+            for (int row = 0; row < size; row++)
+            {
+                for (int col = 0; col < size; col++)
+                {
+                    board[row, col] = ".";
+                }
+            }
+
+            // Отрисовываем поле в консоли
+            Console.WriteLine("   a b c d e f g h i");
+            for (int row = 0; row < size; row++)
+            {
+                Console.Write($"{row + 1} ");
+                for (int col = 0; col < size; col++)
+                {
+                    Console.Write($" {board[row, col]}");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
