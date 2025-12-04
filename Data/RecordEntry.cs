c8a1a2a47f02317dddc0ea09136595044b965181
sofixlu <sofia.luky@icloud.com>
2025-12-04 17:18:03 +0300

    Добавила запись рекордов RecordEntry

---
+++ Data/RecordEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shogi.Data
+{
+    public class RecordEntry
+    {
+        public string Name { get; }
+        public int Score { get; }
+
+        public RecordEntry(string name, int score)
+        {
+            Name = name;
+            Score = score;
+        }
+
+        public override string ToString() => $"{Name};{Score}";
+
+        public static RecordEntry FromString(string line)
+        {
+            var parts = line.Split(';');
+            return new RecordEntry(parts[0], int.Parse(parts[1]));
+        }
+    }
+}
